fff4ccd89c411eb805cb68fa282c3ea2e7c684b5
s16600 <61659844+s16600@users.noreply.github.com>
2020-03-30 20:20:48 +0200

    Zad 4

--- CW3/Controllers/StudentsController.cs
+++ CW3/Controllers/StudentsController.cs
@@ -22,5 +22,19 @@ namespace CW3.Controllers
         {
             return "Kowalski, Malewski, Andrzejewski";
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
     }
 }
