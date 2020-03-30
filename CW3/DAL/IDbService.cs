824299513ad3476ca0251fa38c57056871076485
s16600 <61659844+s16600@users.noreply.github.com>
2020-03-30 21:58:36 +0200

    Zadanie 8

---
+++ CW3/DAL/IDbService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CW3.Controllers;
+
+namespace CW3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
