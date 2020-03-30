634e93ae7835d3b4ca2bf65fb564a8ef3c31a7ae
s16600 <61659844+s16600@users.noreply.github.com>
2020-03-30 19:19:39 +0200

    v1

---
+++ CW3/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        [HttpGet]
+
+        public string GetStudent()
+        {
+            return "Kowalski, Majeski, Andrzejewski";
+        }
+
+    }
+}
